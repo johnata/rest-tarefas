4fbdcb4ab3278bd5acb222b3ce1f7699c4a8e68f
Johnata <johnata@gmail.com>
2017-01-03 23:33:43 -0200

    Add files via upload

---
+++ 02-session-cookie.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    header("Location: http://www.google.com");
+    exit();
+} elseif (isset($_COOKIE['Loggedin']) && $_COOKIE['Loggedin'] == true) {
+    header("Location: http://www.google.com");
+    exit();
+}
